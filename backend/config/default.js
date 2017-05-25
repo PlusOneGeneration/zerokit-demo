de7b228dfc970a49be3cc47a2797690e5a0559f0
Dmitry Ryazantsev <dsr.workk@gmail.com>
2017-05-25 12:27:43 +0300

    - added zerokit config to config files

--- backend/config/default.js
+++ backend/config/default.js
@@ -3,4 +3,16 @@ module.exports = {
     mongo: {
         url: 'mongodb://localhost/zero-kit'
     },
+    zeroKit: {
+        "serviceUrl": "https://s7g8gjvuj7.api.tresorit.io",
+        "adminUserId": "admin@s7g8gjvuj7.tresorit.io",
+        "adminKey": "22a9b060481733f41a2542c1220fd8ef53f0cfa020d8a02f775f0ef3932261bd",
+        "sdkVersion": "4",
+        "idp": [
+            {
+                "clientID": "s7g8gjvuj7_8XWEmNCNox",
+                "clientSecret": "s7g8gjvuj7_dpnIwFp7i3qpUwIh"
+            }
+        ]
+    }
 }
